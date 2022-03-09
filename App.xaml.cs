0af5e41472e82ec89d12077e9b53156ab1c4b970
Lars Behl <lars.behl@gmx.de>
2022-03-09 16:49:08 +0100

    added repro

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestFlyoutApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new SideNav();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace TestFlyoutApp;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnButtonClicked(object sender, EventArgs e)
+    {
+        await Task.Delay(1000);
+        this.MainStackLayout.Add(new Label() { Text = "New Label" });
+    }
+}
+
